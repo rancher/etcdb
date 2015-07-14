4c553617b610bc12347d28e197a0115b458c169f
Matt Good <matt@matt-good.net>
2015-07-14 16:31:19 -0700

    Add CLI flags and usage info

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -17,15 +19,58 @@ import (
 	"github.com/rancherio/etcdb/restapi/operations"
 )
 
+var initDb = flag.Bool("init-db", false, "Initialize the DB schema and exit.")
+
 func main() {
+	flag.Usage = func() {
+		executable := os.Args[0]
+		cmd := filepath.Base(executable)
+
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n\n", executable)
+		fmt.Fprintf(os.Stderr, "  %s [-init-db] <postgres|mysql> <datasource>\n\n", cmd)
+		flag.PrintDefaults()
+
+		fmt.Fprintln(os.Stderr, "\n  Examples:")
+		fmt.Fprintf(os.Stderr, "    %s postgres \"user=username password=password host=hostname dbname=dbname sslmode=disable\"\n", cmd)
+		fmt.Fprintf(os.Stderr, "    %s mysql username:password@tcp(hostname:3306)/dbname\n", cmd)
+
+		fmt.Fprintln(os.Stderr, "\n  Datasource formats:")
+		fmt.Fprintln(os.Stderr, "    postgres: https://godoc.org/github.com/lib/pq#hdr-Connection_String_Parameters")
+		fmt.Fprintln(os.Stderr, "    mysql: https://github.com/go-sql-driver/mysql#dsn-data-source-name")
+	}
+
+	flag.Parse()
+	if flag.NArg() != 2 {
+		flag.Usage()
+		os.Exit(2)
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "0"
+		port = "4001"
 	}
 
 	host := os.Getenv("HOST")
 	if host == "" {
 		host = "localhost"
+	}
+
+	dbDriver := flag.Arg(0)
+	dbDataSource := flag.Arg(1)
+
+	fmt.Println("connecting to database:", dbDriver, dbDataSource)
+	store, err := backend.New(dbDriver, dbDataSource)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if *initDb {
+		fmt.Println("initializing db schema...")
+		err = store.CreateSchema()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		return
 	}
 
 	listener, err := net.Listen("tcp", host+":"+port)
@@ -34,8 +79,6 @@ func main() {
 	}
 
 	fmt.Printf("serving etcd at http://%s\n", listener.Addr())
-
-	store, _ := backend.New("postgres", "sslmode=disable")
 
 	r := mux.NewRouter()
 
@@ -73,6 +116,7 @@ func main() {
 			if _, ok := err.(models.Error); ok {
 				return err
 			} else if err != nil {
+				log.Println(err)
 				return models.RaftInternalError(err.Error())
 			}
 
